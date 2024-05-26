215ed06fb7028047cd3987460053b680c03ceacc
Stefan Kondratschin <kondratschin@gmail.com>
2024-05-26 12:36:32 +0200

    added cards

--- pokedex.js
+++ pokedex.js
@@ -6,3 +6,26 @@ async function loadData() {
     console.log(responseToJson);
 };
 
+let MON_URL = "https://pokeapi.co/api/v2/pokemon/1/";
+
+async function loadDataMon() {
+    let response = await fetch(MON_URL);
+    let responseToJson = await response.json();
+    let monPic = responseToJson.sprites.other.home.front_default;
+    let monTyp = responseToJson.types;
+    let monName = responseToJson.species.name;
+    let monNo = responseToJson.order;
+    console.log(responseToJson);
+    console.log(monPic);
+    console.log(monTyp);
+    console.log(monName);
+    console.log(monNo);
+};
+
+
+
+// function fillCard() {
+//     let monPic = 
+// }
+
+// "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",

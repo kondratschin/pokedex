f95adf0733b70e21a3dc6ea8c3ccebfac2d48d13
Stefan Kondratschin <kondratschin@gmail.com>
2024-05-27 21:21:07 +0200

    added pictures

--- pokedex.js
+++ pokedex.js
@@ -1,29 +1,31 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=10";
 
 
 async function loadData() {
     let response = await fetch(BASE_URL + ".json");
     let responseToJson = await response.json();
-    console.log(responseToJson.results.length);
+    console.log(responseToJson.results);
     return responseToJson.results.length;
+
 };
 
-let MON_URL = "https://pokeapi.co/api/v2/pokemon/1/";
+let MON_URL = "https://pokeapi.co/api/v2/pokemon/4/";
 
 async function loadDataMon() {
     let response = await fetch(MON_URL);
     let responseToJson = await response.json();
     let monPic = responseToJson.sprites.other.home.front_default;
-    let monTyp = responseToJson.types;
+    let monTyp1 = responseToJson.types[0].type.name;
+    let monTyp2 = responseToJson.types[1].type.name;
     let monName = responseToJson.species.name;
     let monNo = responseToJson.order;
     console.log(responseToJson);
     console.log(monPic);
-    console.log(monTyp);
+    console.log(monTyp1);
+    console.log(monTyp2);
     console.log(monName);
     console.log(monNo);
 };
-
 
 
 async function fillCard() {
@@ -35,11 +37,14 @@ async function fillCard() {
     for (let index = 1; index <= monAmount; index++) {
         let MON_URL = `https://pokeapi.co/api/v2/pokemon/${index}/`;
         let response = await fetch(MON_URL);
-        let data = await response.json();
-        let monPic = data.sprites.other.home.front_default;
-        let monName = data.species.name;
-        let monNo = data.id;
+        let pokemon = await response.json();
+        let monPic = pokemon.sprites.other.home.front_default;
+        let monName = pokemon.species.name;
+        let monTypeOne = pokemon.types[0].type.name;
+        let monTypeTwo = pokemon.types[1] ? pokemon.types[1].type.name : null; // Check for second type
+        let monNo = pokemon.id;
         monName = monName.charAt(0).toUpperCase() + monName.slice(1);
+
         let card = `
         <div class="card">
             <div class="card-header">
@@ -48,9 +53,8 @@ async function fillCard() {
             </div>
             <div class="card-image" style="background-image: url('${monPic}');"></div>
             <div class="card-icons">
-                <img src="https://via.placeholder.com/30" alt="Icon 1">
-                <img src="https://via.placeholder.com/30" alt="Icon 2">
-                <img src="https://via.placeholder.com/30" alt="Icon 3">
+                <div class="types" id="type-one" style="background-image: url('img/${monTypeOne}.svg');"></div>
+                ${monTypeTwo ? `<div class="types" id="type-two" style="background-image: url('img/${monTypeTwo}.svg');"></div>` : ''}
             </div>
         </div>
         `;
